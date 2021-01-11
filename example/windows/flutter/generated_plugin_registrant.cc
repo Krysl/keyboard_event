3554d5223f2febc9c22e2fd4144d881891f2827b
Krysl <krysl@qq.com>
2021-01-12 01:24:27 +0800

    init with `flutter create -t plugin --platforms windows keyboard_event`
    Signed-off-by: Krysl <krysl@qq.com>

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <keyboard_event/keyboard_event_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  KeyboardEventPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("KeyboardEventPlugin"));
+}
---
+++ windows/keyboard_event_plugin.cpp
@@ -0,0 +1,82 @@
+#include "include/keyboard_event/keyboard_event_plugin.h"
+
+// This must be included before many other Windows headers.
+#include <windows.h>
+
+// For getPlatformVersion; remove unless needed for your plugin implementation.
+#include <VersionHelpers.h>
+
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+#include <map>
+#include <memory>
+#include <sstream>
+
+namespace {
+
+class KeyboardEventPlugin : public flutter::Plugin {
+ public:
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+
+  KeyboardEventPlugin();
+
+  virtual ~KeyboardEventPlugin();
+
+ private:
+  // Called when a method is called on this plugin's channel from Dart.
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+};
+
+// static
+void KeyboardEventPlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "keyboard_event",
+          &flutter::StandardMethodCodec::GetInstance());
+
+  auto plugin = std::make_unique<KeyboardEventPlugin>();
+
+  channel->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto &call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+KeyboardEventPlugin::KeyboardEventPlugin() {}
+
+KeyboardEventPlugin::~KeyboardEventPlugin() {}
+
+void KeyboardEventPlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
+    }
+    result->Success(flutter::EncodableValue(version_stream.str()));
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace
+
+void KeyboardEventPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  KeyboardEventPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+}
