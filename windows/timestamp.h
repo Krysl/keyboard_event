d347f05bde468298fcfd0cbc3cc6ed81bc76980a
Krysl <krysl@qq.com>
2021-01-20 16:19:57 +0800

    优化时间戳生成，添加/utf-8编译选项

--- windows/timestamp.h
+++ windows/timestamp.h
@@ -1,6 +1,6 @@
 #ifndef __TIMESTAMP_H__ 
 #define __TIMESTAMP_H__ 
-#define __TIMESTAMP 2021/01/19 周二 22:02:19.20 
+#define __TIMESTAMP 2021/01/20 周三 16:18:33.34 
 #define STR(x) #x 
 #define _TIMESTAMP(x) STR(x) 
 #define TIMESTAMP _TIMESTAMP(__TIMESTAMP) 
