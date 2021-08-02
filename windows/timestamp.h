a405b8ec75e6be8ab62c91cdf87a2e71b8daf132
Krysl <krysl@qq.com>
2021-08-02 18:57:19 +0800

    try fix spdlog problem
    Signed-off-by: Krysl <krysl@qq.com>

--- windows/timestamp.h
+++ windows/timestamp.h
@@ -1,6 +1,6 @@
 #ifndef __TIMESTAMP_H__ 
 #define __TIMESTAMP_H__ 
-#define __TIMESTAMP 2021/01/24 周日 17:39:47.61 
+#define __TIMESTAMP 2021/08/02 周一 18:46:07.68 
 #define STR(x) #x 
 #define _TIMESTAMP(x) STR(x) 
 #define TIMESTAMP _TIMESTAMP(__TIMESTAMP) 
