fac154cf33f7d34f75ec008c37f77f92bb1e8848
Krysl <krysl@qq.com>
2021-08-02 19:25:23 +0800

    fix cmake config
    Signed-off-by: Krysl <krysl@qq.com>

--- windows/timestamp.h
+++ windows/timestamp.h
@@ -1,6 +1,6 @@
 #ifndef __TIMESTAMP_H__ 
 #define __TIMESTAMP_H__ 
-#define __TIMESTAMP 2021/08/02 周一 18:46:07.68 
+#define __TIMESTAMP 2021/08/02 周一 19:11:04.00 
 #define STR(x) #x 
 #define _TIMESTAMP(x) STR(x) 
 #define TIMESTAMP _TIMESTAMP(__TIMESTAMP) 
