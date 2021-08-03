3f15c289b0e77c1c8b0e2a0cc2bd1542390dc3b2
Krysl <krysl@qq.com>
2021-08-03 18:12:05 +0800

    add debug support for code not in git repository
    Signed-off-by: Krysl <krysl@qq.com>

--- windows/timestamp.h
+++ windows/timestamp.h
@@ -1,6 +1,6 @@
 #ifndef __TIMESTAMP_H__ 
 #define __TIMESTAMP_H__ 
-#define __TIMESTAMP 2021/08/03 周二  4:55:35.31 
+#define __TIMESTAMP 2021/08/03 周二 18:11:15.53 
 #define STR(x) #x 
 #define _TIMESTAMP(x) STR(x) 
 #define TIMESTAMP _TIMESTAMP(__TIMESTAMP) 
